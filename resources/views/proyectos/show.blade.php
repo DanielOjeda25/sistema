a031eae0f6bf4d70264cf361d2a9ff3fec997835
DanielOjeda25 <88939356+DanielOjeda25@users.noreply.github.com>
2026-09-02 17:02:34 -0300

    Condicionar acciones de edición por rol

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -29,7 +29,9 @@
                 </div>
 
                 <div class="pt-4 flex gap-4 border-t">
+                    @hasanyrole('Jefe|PM')
                     <a href="{{ route('proyectos.edit', $proyecto) }}" class="text-yellow-600 hover:underline">Editar</a>
+                    @endhasanyrole
                     <a href="{{ route('proyectos.index') }}" class="text-gray-600 hover:underline">Volver al listado</a>
                 </div>
 
--- resources/views/solicitudes_cambio/show.blade.php
+++ resources/views/solicitudes_cambio/show.blade.php
@@ -28,7 +28,9 @@
                 </div>
 
                 <div class="pt-4 flex gap-4 border-t">
+                    @hasanyrole('Jefe|PM|PO')
                     <a href="{{ route('solicitudes-cambio.edit', $solicitud) }}" class="text-yellow-600 hover:underline">Editar</a>
+                    @endhasanyrole
                     <a href="{{ route('solicitudes-cambio.index') }}" class="text-gray-600 hover:underline">Volver al listado</a>
                 </div>
 
